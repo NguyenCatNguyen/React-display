e6e54c72786b3a007cdd722cd8494b9ad3f3ec18
NguyenCatNguyen <catngn11@gmail.com>
2025-02-11 23:21:50 -0600

    update

---
+++ ZeldaWiki/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+import Welcome from "./components/Welcome.jsx"
+
+function App() {
+  return (
+    <div className="bg-black">
+      <Welcome/>
+    </div>
+  )
+}
+
+export default App
