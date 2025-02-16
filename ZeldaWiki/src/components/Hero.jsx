6946d45677dcfd978147676cc2308d694f5a71f7
NguyenCatNguyen <catngn11@gmail.com>
2025-02-15 19:42:20 -0600

    update

--- ZeldaWiki/src/components/Hero.jsx
+++ ZeldaWiki/src/components/Hero.jsx
@@ -7,20 +7,21 @@ const Hero = () => {
       <div className="flex text-2xl justify-between ">
         <a className='flex' 
           href="https://github.com/NguyenCatNguyen/React-display/tree/main/ZeldaWiki">
-          <div className='font-bold blue '>ZeldaWiki</div>
+          <div className='font-bold blue border '>ZeldaWiki</div>
         </a>
         <div className="side-bar gold">
-          <a className="mr-4 point" href="">Search</a>
-          <a href="">Favorite</a>
+          <a href="" className="border mr-2 ">Search</a>
+          <a href="" className="border">Favorite</a>
         </div>
       </div>
-        {/* <div className='flex '>
-          <img src={hero} alt='Link image' className='h-fit'/>
-          <h2 className='text-8xl font-bold text-left w-[600px]'>
+
+        <div className='flex mt-5 '>
+          <h2 className='text-8xl font-bold text-left'>
             Find the <span className='underline decoration-blue-300'>detail</span> you looking for thought<span className='text-blue-300'> Zelda Wiki</span>
           </h2>
+          <img src={hero} alt='Link image' className='rounded-xl w-sm'/>
           
-        </div> */}
+        </div>
     </div>
   )
 }
